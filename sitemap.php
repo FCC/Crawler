25b50a3d6b34f78325c987bc08ad5c7719c5bc96
Greg Elin <greg@fotonotes.net>
2012-06-03 10:38:26 -0400

    Put <?xml tag into echo statement to avoid error when using php short tags.

--- sitemap.php
+++ sitemap.php
@@ -1,5 +1,5 @@
 <?php header ("Content-Type:text/xml");?>
-<?xml version="1.0" encoding="UTF-8"?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <?php 
 include('config.php');
